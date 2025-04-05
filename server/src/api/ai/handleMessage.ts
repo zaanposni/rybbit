28795330ce0c7792f07653ac0673b4c00316494f
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2025-04-05 05:47:56 -0700

    Create basic stateless ReAct analytics agent

--- server/src/api/ai/handleMessage.ts
+++ server/src/api/ai/handleMessage.ts
@@ -2,18 +2,16 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { getUserHasAccessToSite } from "../../lib/auth-utils.js";
 import { initChatModel } from "langchain/chat_models/universal";
 import { generateAnalyticsTools } from "./tools.js";
-
-interface Message {
-  role: "user" | "system";
-  content: string;
-}
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { analyticsAgentSystemPrompt } from "./prompts.js";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
 
 interface HandleMessageRequest {
   Body: {
     site: string;
     timezone: string;
     date: string;
-    messages: Message[];
+    message: string;
   };
 }
 
@@ -21,7 +19,7 @@ export async function handleMessage (
   req: FastifyRequest<HandleMessageRequest>,
   res: FastifyReply
 ) {
-  const { site, timezone, date, messages } = req.body;
+  const { site, timezone, date, message } = req.body;
 
   const userHasAccessToSite = await getUserHasAccessToSite(req, site);
   if (!userHasAccessToSite) {
@@ -33,8 +31,21 @@ export async function handleMessage (
       modelProvider: process.env.PROVIDER,
       temperature: 0,
     });
-    const llmWithTools = llm.bindTools(generateAnalyticsTools(timezone, site));
+    const tools = generateAnalyticsTools(timezone, site);
+    const analyticsAgent = createReactAgent({ llm, tools });
+
+    const promptTemplate = ChatPromptTemplate.fromMessages([
+      ["system", analyticsAgentSystemPrompt],
+      // ["placeholder", "{messages}"],
+      ["user", message],
+    ]);
+    const prompt = await promptTemplate.invoke({ date });
+
+    const result = await analyticsAgent.invoke(prompt);
+
+    return res.send({ response: result.messages[result.messages.length - 1].content });
   } catch (error) {
     console.error(error);
+    return res.status(500).send({ error: "Failed to handle message" });
   }
 }
